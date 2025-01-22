54b12918926a574588b171630b28d63753e27a6d
HabibiHadron <102247412+HabibiHadron@users.noreply.github.com>
2025-01-22 15:43:26 +0500

    Update HabibiModAnalyzer.ps1

--- HabibiModAnalyzer.ps1
+++ HabibiModAnalyzer.ps1
@@ -1,3 +1,4 @@
+Clear-Host
 Write-Host "Habibi Mod Analyzer" -ForegroundColor Yellow
 Write-Host "Made by " -ForegroundColor DarkGray -NoNewline
 Write-Host "HadronCollision"
@@ -9,10 +10,23 @@ $mods = Read-Host "Path"
 Write-Host ""
 
 if (-not $mods) {
-    $mods = Join-Path -Path $env:APPDATA -ChildPath ".minecraft\mods"
+    $mods = "$env:USERPROFILE\AppData\Roaming\.minecraft\mods"
 	Write-Host "Press enter to continue with " -ForegroundColor DarkYellow -NoNewline
 	Write-Host $mods -ForegroundColor DarkGray
 	Read-Host
+}
+
+$process = Get-Process javaw -ErrorAction SilentlyContinue
+
+if ($process) {
+    $startTime = $process.StartTime
+    
+    $elapsedTime = (Get-Date) - $startTime
+    
+    Write-Host "Minecraft (javaw.exe) Process ID: $($process.Id)" -ForegroundColor DarkYellow
+    Write-Host "Started at: $startTime"
+    Write-Host "Running for: $($elapsedTime.Hours)h $($elapsedTime.Minutes)m $($elapsedTime.Seconds)s"
+    Write-Host ""
 }
 
 function Get-FileHashSHA1 {
@@ -27,6 +41,7 @@ function Get-FileHashSHA1 {
         $fileStream.Close()
     }
     $hash = [BitConverter]::ToString($hashBytes).Replace("-", "")
+
     return $hash
 }
 
@@ -82,6 +97,45 @@ Get-ChildItem -Path $mods -Filter *.jar | ForEach-Object {
 
 if ($unknownMods.Count -gt 0) {
     Write-Host ""
-    Write-Host "Suspicious Mods:" -ForegroundColor Yellow
+    Write-Host "Suspicious mods found:" -ForegroundColor Yellow
     $unknownMods | ForEach-Object { Write-Host "- $_" -ForegroundColor Red }
+}
+
+$bannedMods = @(
+    @{ name = "Wurst Client"; value = "- wurst" },
+    @{ name = "Meteor Client"; value = "- meteor-client" },
+    @{ name = "Francium Client"; value = "- org_apache"},
+    @{ name = "Doomsday Client"; value = "- dd"},
+	@{ name = "Prestige Client"; value = "- prestige"},
+    @{ name = "Walksy Optimizer"; value = "- walksycrystaloptimizer" },
+	@{ name = "Walksy Shield Statuses"; value = "- shieldstatus" },
+    @{ name = "Accurate Block Placement"; value = "- accurateblockplacement" },
+    @{ name = "Elytra Chest Swapper"; value = "- ecs" },
+    @{ name = "Click Crystals"; value = "- clickcrystals" },
+    @{ name = "Auto Totem"; value = "- autototem" },
+    @{ name = "Item Scroller"; value = "- itemscroller" },
+    @{ name = "Tweakeroo"; value = "- tweakeroo" },
+    @{ name = "Mouse Tweaks"; value = "- mousetweaks" },
+    @{ name = "Freecam"; value = "- freecam" },
+	@{ name = "Xaero's Minimap"; value = "- xaerominimap" },
+    @{ name = "HitRange"; value = "- hitrange" }
+)
+
+$logPath = "$env:USERPROFILE\AppData\Roaming\.minecraft\logs\latest.log"
+$fileContent = Get-Content -Path $logPath
+
+$logMods = @()
+foreach ($line in $fileContent) {
+    foreach ($mod in $bannedMods) {
+        if ($line -match $mod.value) {
+            $logMods += $mod.name
+            break
+        }
+    }
+}
+
+if ($logMods.Count -gt 0) {
+    Write-Host ""
+    Write-Host "Possibly bannable mods in /.minecraft/logs/latest.log:" -ForegroundColor Yellow
+    $logMods | ForEach-Object { Write-Host "- $_" -ForegroundColor Red }
 }
